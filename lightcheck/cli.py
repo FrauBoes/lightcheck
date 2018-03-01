a4b1b3fa264685489dac02638c5ab1b16ef82534
JuliaBoes <juliaboes@posteo.de>
2018-03-01 18:29:00 +0000

    add skeleton in cli. script

--- lightcheck/cli.py
+++ lightcheck/cli.py
@@ -3,13 +3,22 @@
 """Console script for lightcheck."""
 
 import click
-
+click.disable_unicode_literals_warning = True
 
 @click.command()
 @click.option("--input", default=None, help="input URI (file or URL)")
 def main(input=None):
     """Console script for lightcheck."""
     print("input", input)
+    
+    N, instructions = parseFile(input)
+    
+    lightChecker = LightChecker(N)
+    
+    for instruction in instructions:
+        lightChecker.apply(instruction)
+        
+    print('# lights occupied: ', lightChecker.countOccupied())
     return 0
 
 
